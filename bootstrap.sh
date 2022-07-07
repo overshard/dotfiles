6a62b008abc0da0045db2c55422b1c3c6105ce3c
Isaac Bythewood <isaac@bythewood.me>
2022-07-07 16:10:08 +0000

    Add spacevim config

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,10 @@
 cd ~; git clone https://github.com/overshard/dotfiles .dotfiles
 
 
+# Install SpaceVim to have a full IDE via the terminal if required
+
 curl -o- https://spacevim.org/install.sh | bash
+
 
 # Symlink all configuration files
 
@@ -20,3 +23,7 @@ echo "source ~/.bash_aliases" >> ~/.bashrc
 # vscode
 mkdir -p ~/.vscode-server/data/Machine
 ln -s ~/.dotfiles/vscode/settings.json ~/.vscode-server/data/Machine/settings.json
+
+# spacevim
+mkdir -p ~/.SpaceVim.d
+ln -s ~/.dotfiles/.SpaceVim.d/init.toml ~/.SpaceVim.d/init.toml
