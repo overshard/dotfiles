ddb802b04c8587b65b5874a09f112f7d7655f9f6
Isaac Bythewood <isaac@bythewood.me>
2022-07-07 01:21:52 +0000

    Use spacevim instead of a custom nvim config

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,14 +6,12 @@
 cd ~; git clone https://github.com/overshard/dotfiles .dotfiles
 
 
+curl -o- https://spacevim.org/install.sh | bash
+
 # Symlink all configuration files
 
 # git
 ln -s ~/.dotfiles/git/gitconfig .gitconfig
-
-# nvim
-mkdir -p ~/.config/nvim
-ln -s ~/.dotfiles/nvim/init.vim .config/nvim/init.vim
 
 # bash
 ln -s ~/.dotfiles/bash/bash_aliases .bash_aliases
